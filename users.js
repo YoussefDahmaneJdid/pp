4ead0fe04869ff365d7eaf38330bfe73fa0c776b
YoussefDahmaneJdid <54043642+YoussefDahmaneJdid@users.noreply.github.com>
2019-09-25 11:54:22 +0200

    Update users.js

--- users.js
+++ users.js
@@ -139,6 +139,7 @@ router.post('/', function(req, res, next) {
 
   db.connexion.query(queryUserPerId,(er,row,field)=>{
     const queryUser = "select *  from users u where u.id_user !=? "
+    console.log(er);
     if(!er)
     if(row.length){
     if(row[0].token==token)
